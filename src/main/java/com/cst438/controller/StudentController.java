f34a351826f8b6f93b564479a937637e11b8bbce
suhaibperacha <speracha@csumb.edu>
2026-07-19 18:58:38 -0700

    Add path for viewing student transcript

--- src/main/java/com/cst438/controller/StudentController.java
+++ src/main/java/com/cst438/controller/StudentController.java
@@ -2,7 +2,6 @@ package com.cst438.controller;
 
 import com.cst438.domain.*;
 import com.cst438.dto.EnrollmentDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -15,83 +14,127 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-   private final EnrollmentRepository enrollmentRepository;
-   private final UserRepository userRepository;
+    private final EnrollmentRepository enrollmentRepository;
+    private final UserRepository userRepository;
 
-   public StudentController(
-           EnrollmentRepository enrollmentRepository,
-           UserRepository userRepository
-   ) {
-       this.enrollmentRepository = enrollmentRepository;
-       this.userRepository = userRepository;
-   }
+    public StudentController(
+            EnrollmentRepository enrollmentRepository,
+            UserRepository userRepository
+    ) {
+        this.enrollmentRepository = enrollmentRepository;
+        this.userRepository = userRepository;
+    }
 
-   // retrieve schedule for student for a term
-   @GetMapping("/enrollments")
-   @PreAuthorize("hasAuthority('SCOPE_ROLE_STUDENT')")
-   public List<EnrollmentDTO> getSchedule(
-           @RequestParam("year") int year,
-           @RequestParam("semester") String semester,
-           Principal principal) {
-			   
-		// use the EnrollmentController findByYearAndSemsterOrderByCourseId
-		// method to retrive the enrollments given the year, semester and id 
-		// of the logged in student.
-		// Return a list of EnrollmentDTO.
-       User student = userRepository.findByEmail(principal.getName());
+    // retrieve schedule for student for a term
+    @GetMapping("/enrollments")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_STUDENT')")
+    public List<EnrollmentDTO> getSchedule(
+            @RequestParam("year") int year,
+            @RequestParam("semester") String semester,
+            Principal principal) {
 
-       if (student == null) {
-           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid student email");
-       }
+        // use the EnrollmentController findByYearAndSemsterOrderByCourseId
+        // method to retrive the enrollments given the year, semester and id
+        // of the logged in student.
+        // Return a list of EnrollmentDTO.
+        User student = userRepository.findByEmail(principal.getName());
 
-       List<Enrollment> enrollments =
-               enrollmentRepository.findByYearAndSemesterOrderByCourseId(
-                       year,
-                       semester,
-                       student.getId()
-               );
+        if (student == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "invalid student email"
+            );
+        }
 
-       List<EnrollmentDTO> dtoList = new ArrayList<>();
+        List<Enrollment> enrollments =
+                enrollmentRepository.findByYearAndSemesterOrderByCourseId(
+                        year,
+                        semester,
+                        student.getId()
+                );
 
-       for (Enrollment enrollment : enrollments) {
-           Section section = enrollment.getSection();
-           Course course = section.getCourse();
-           Term term = section.getTerm();
+        List<EnrollmentDTO> dtoList = new ArrayList<>();
 
-           EnrollmentDTO dto = new EnrollmentDTO(
-                   enrollment.getEnrollmentId(),
-                   enrollment.getGrade(),
-                   student.getId(),
-                   student.getName(),
-                   student.getEmail(),
-                   course.getCourseId(),
-                   course.getTitle(),
-                   section.getSectionId(),
-                   section.getSectionNo(),
-                   section.getBuilding(),
-                   section.getRoom(),
-                   section.getTimes(),
-                   course.getCredits(),
-                   term.getYear(),
-                   term.getSemester()
-           );
+        for (Enrollment enrollment : enrollments) {
+            Section section = enrollment.getSection();
+            Course course = section.getCourse();
+            Term term = section.getTerm();
 
-           dtoList.add(dto);
-       }
+            EnrollmentDTO dto = new EnrollmentDTO(
+                    enrollment.getEnrollmentId(),
+                    enrollment.getGrade(),
+                    student.getId(),
+                    student.getName(),
+                    student.getEmail(),
+                    course.getCourseId(),
+                    course.getTitle(),
+                    section.getSectionId(),
+                    section.getSectionNo(),
+                    section.getBuilding(),
+                    section.getRoom(),
+                    section.getTimes(),
+                    course.getCredits(),
+                    term.getYear(),
+                    term.getSemester()
+            );
 
-       return dtoList;
-   }
+            dtoList.add(dto);
+        }
 
-   // return transcript for student
+        return dtoList;
+    }
+
+    // return transcript for student
     @GetMapping("/transcripts")
     @PreAuthorize("hasAuthority('SCOPE_ROLE_STUDENT')")
     public List<EnrollmentDTO> getTranscript(Principal principal) {
 
         // use the EnrollmentController findEnrollmentsByStudentIdOrderByTermId
-		// method to retrive the enrollments given the id 
-		// of the logged in student.
-		// Return a list of EnrollmentDTO.
-		
-        return null;
+        // method to retrive the enrollments given the id
+        // of the logged in student.
+        // Return a list of EnrollmentDTO.
+        User student = userRepository.findByEmail(principal.getName());
+
+        if (student == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "invalid student email"
+            );
+        }
+
+        List<Enrollment> enrollments =
+                enrollmentRepository.findEnrollmentsByStudentIdOrderByTermId(
+                        student.getId()
+                );
+
+        List<EnrollmentDTO> dtoList = new ArrayList<>();
+
+        for (Enrollment enrollment : enrollments) {
+            Section section = enrollment.getSection();
+            Course course = section.getCourse();
+            Term term = section.getTerm();
+
+            EnrollmentDTO dto = new EnrollmentDTO(
+                    enrollment.getEnrollmentId(),
+                    enrollment.getGrade(),
+                    student.getId(),
+                    student.getName(),
+                    student.getEmail(),
+                    course.getCourseId(),
+                    course.getTitle(),
+                    section.getSectionId(),
+                    section.getSectionNo(),
+                    section.getBuilding(),
+                    section.getRoom(),
+                    section.getTimes(),
+                    course.getCredits(),
+                    term.getYear(),
+                    term.getSemester()
+            );
+
+            dtoList.add(dto);
+        }
+
+        return dtoList;
     }
 }
